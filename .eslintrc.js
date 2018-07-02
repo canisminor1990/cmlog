c223652e7650e57b4d581d155f25a9629647c8ba
canisminor1990 <canisminor1990@gmail.com>
2018-07-02 13:28:30 +0800

    root

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	parser: 'babel-eslint',
+	extends: [
+		'standard',
+		'prettier',
+		'prettier/standard'
+	],
+	plugins: [
+		'prettier',
+		'standard'
+	],
+	parserOptions: {
+		'ecmaFeatures': {
+			'experimentalObjectRestSpread': true,
+		}
+	},
+	env: {
+		'es6': true,
+		'node': true
+	},
+	rules: {
+		'prettier/prettier': [
+			2, {
+				'printWidth': 100,
+				'singleQuote': true,
+				'trailingComma': 'es5',
+				'parser': 'flow',
+			}
+		]
+	}
+};
