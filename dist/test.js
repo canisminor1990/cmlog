4bc33af6d89511caf3e09627c26badd0b7b041a9
canisminor1990 <canisminor1990@gmail.com>
2018-07-02 14:35:39 +0800

    update

--- dist/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var cmlog = require('./index');
-
-cmlog.debug('export something');
-cmlog.warn('Something is going to happen soon');
-cmlog.success('Build succeeed in 10 seconds');
-cmlog.info('Some extra info is here');
-cmlog.error(new Error('Foo'));
